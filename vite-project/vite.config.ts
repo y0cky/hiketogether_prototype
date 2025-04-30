5ae3273c927df484a6730ef6d31849f26f27f19a
2024025 <2024025@stud.hs-mannheim.de>
2025-04-30 19:39:11 +0200

    commit

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/hiketogether_prototype/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/hiketogether_prototype/' : '/', // 🔥 wichtig!
   plugins: [
     react(),
     tailwindcss()
