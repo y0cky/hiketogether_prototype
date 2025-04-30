6f76a62f301046c8fc1b9e5223467d26a3754b2e
2024025 <2024025@stud.hs-mannheim.de>
2025-04-30 19:12:46 +0200

    commit

--- vite-project/src/pages/Home.tsx
+++ vite-project/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Infobar from '../components/Infobar'
 import Header from '../components/Header'
 import TourSection from '../components/TourSection'
 import '../styles/index.css'
-import '../styles/app.css'
+import '../styles/App.css'
 
 
 export default function Home() {
